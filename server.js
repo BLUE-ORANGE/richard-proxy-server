15df8df1e696b47ee8ef83665dc385c27a766ace
Richard Hai <hirichardhai@gmail.com>
2018-04-10 16:00:54 -0700

    updated proxy server to render andrew richard and trevors component

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(morgan('dev'));
 // app.use('v1/product/:id/reviews', proxy('localhost:3004/v1/product/:id/reviews'));
 // app.use('v1/product/:id/summary', proxy('localhost:3002/v1/product/:id/summary'));
 // app.use('v1/product/:id/images', proxy('localhost:3003/v1/product/:id/'));
-// app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use('/v1/product/:id', express.static(path.join(__dirname, '/public')));
 
 app.listen(port, () => {
